d3095df47aaf346bebca33bb3b3fe04e30bd08c1
Anselem <anselemodims@gmail.com>
2022-01-03 14:58:17 +0100

    Feat: created the Calculator class

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">Hello App</div>
-  );
-}
-
-export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator">Hello App</div>
+    );
+  }
+}
+
+export default Calculator;
