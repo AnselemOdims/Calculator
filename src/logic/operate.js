63fe0fd7aba39393c39b3a4a441e4e0c27061785
Anselem <anselemodims@gmail.com>
2022-01-04 15:34:22 +0100

    Feat: changed the if to switch statements

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,28 +3,26 @@ import Big from 'big.js';
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne);
   const two = Big(numberTwo);
-  if (operation === '+') {
-    return one.plus(two).toString();
+  let result;
+  switch (operation) {
+    case '+':
+      result = one.plus(two).toString();
+      break;
+    case '-':
+      result = one.minus(two).toString();
+      break;
+    case 'x':
+      result = one.times(two).toString();
+      break;
+    case '÷':
+      try {
+        result = one.div(two).toString();
+      } catch (e) {
+        throw new Error("Can't find modulo as can't divide by 0.");
+      }
+      break;
+    default:
+      throw Error(`Unknown operation '${operation}'`);
   }
-  if (operation === '-') {
-    return one.minus(two).toString();
-  }
-  if (operation === 'x') {
-    return one.times(two).toString();
-  }
-  if (operation === '÷') {
-    try {
-      return one.div(two).toString();
-    } catch (err) {
-      return "Can't divide by 0.";
-    }
-  }
-  if (operation === '%') {
-    try {
-      return one.mod(two).toString();
-    } catch (err) {
-      return "Can't find modulo as can't divide by 0.";
-    }
-  }
-  throw Error(`Unknown operation '${operation}'`);
+  return result;
 }
