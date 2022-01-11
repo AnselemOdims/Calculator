5d967e2b85acc4baa677f458ad921b6628d33f54
Anselem <anselemodims@gmail.com>
2022-01-11 14:08:54 +0100

    Feat: styled the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { FcCalculator } from 'react-icons/fc';
 
 const Header = () => {
   const links = [
@@ -21,11 +22,24 @@ const Header = () => {
   return (
     <header>
       <div>
+        <FcCalculator size="20px" style={{ marginRight: '8px' }} />
         <h3>Math-Magicians</h3>
       </div>
       <nav>
-        { links.map((link) => (
-          <NavLink key={link.id} to={link.path}>{link.text}</NavLink>
+        {links.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            className="link"
+            style={({ isActive }) => ({
+              color: isActive ? '#f4913f' : '#3b3b41',
+              borderBottom: isActive ? 'solid 4px #f4913f' : '',
+              paddingBottom: isActive ? '5px' : '3px',
+              fontWeight: 550,
+            })}
+          >
+            {link.text}
+          </NavLink>
         ))}
       </nav>
     </header>
