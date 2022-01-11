fceb6de8ee45f385c86c9cdd3d63cb2708472e39
Anselem <anselemodims@gmail.com>
2022-01-11 12:46:49 +0100

    Feat: imported the functions from react -router-dom and included the components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './components/Calculator';
+import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import NotMatch from './pages/NotMatch';
+
+const home = '/calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path={`${home}`} element={<Home />} />
+        <Route path={`${home}/calc`} element={<Calculator />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
